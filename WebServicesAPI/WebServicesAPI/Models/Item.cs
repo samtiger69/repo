a723d28c9e67fa97020e784572b70fd698a608f6
MOMANI1 <mmn1078@gmail.com>
2017-01-29 14:56:15 +0200

    reqired

--- WebServicesAPI/WebServicesAPI/Models/Item.cs
+++ WebServicesAPI/WebServicesAPI/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace WebServicesAPI.Models
     public class Item
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
